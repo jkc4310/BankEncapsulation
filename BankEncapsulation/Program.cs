0ff060328633176c3487dc7c4e6016aa5fe12476
kendall chapman <jkc4310@gmail.com>
2023-08-01 14:02:40 -0400

    Bank Encapsulation exercise

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+
+            var AmountToDeposit = double.Parse(Console.ReadLine());
+
+            account.deposit(AmountToDeposit);
+
+            Console.WriteLine($"Thank you! Your balance is currently {account.GetBalance()}");
         }
     }
 }
